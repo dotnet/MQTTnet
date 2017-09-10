644bcfba2777c9817e5d2a837d91f8863f98d7b1
JanEggers <jan.eggers@ferag.com>
2017-09-10 15:41:39 +0200

    - make MqttPacketReader and Writer a specialized version of their Binary counterparts - improved message framing. as basis to improve buffering - share more common code for the writer part

---
+++ MQTTnet.Core/Packets/MqttPacketHeader.cs
@@ -0,0 +1,13 @@
+﻿using MQTTnet.Core.Protocol;
+
+namespace MQTTnet.Core.Packets
+{
+    public class MqttPacketHeader
+    {
+        public MqttControlPacketType ControlPacketType { get; set; }
+
+        public byte FixedHeader { get; set; }
+
+        public int BodyLength { get; set; }
+    }
+}

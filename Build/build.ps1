e0797e5d14a2ffd54dc31ef05a0931fed2cfaacc
JanEggers <jan.eggers@ferag.com>
2017-10-26 21:45:36 +0200

    enable uwp build again

--- Build/build.ps1
+++ Build/build.ps1
@@ -5,7 +5,7 @@ if ([string]::IsNullOrEmpty($version)) {$version = "0.0.1"}
 $msbuild = "MSBuild.exe"
 &dotnet build ..\Frameworks\MQTTnet.Netstandard\MQTTnet.Netstandard.csproj -c="Release" /p:FileVersion=$version /p:AssemblyVersion=$version
 &dotnet build ..\Frameworks\MQTTnet.AspNetCore\MQTTnet.AspNetCore.csproj -c="Release" /p:FileVersion=$version /p:AssemblyVersion=$version
-##&$msbuild ..\Frameworks\MQTTnet.UniversalWindows\MQTTnet.UniversalWindows.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\Frameworks\MQTTnet.UniversalWindows\MQTTnet.UniversalWindows.csproj /t:Build /p:Configuration="Release"
 
 Remove-Item .\NuGet -Force -Recurse
 New-Item -ItemType Directory -Force -Path .\NuGet

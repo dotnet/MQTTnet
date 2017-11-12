c0f5b90e91e0c370b337ec92d390b1504b452087
JanEggers <jan.eggers@ferag.com>
2017-11-12 12:33:56 +0100

    added testproject for importing mqtt nugets and verify bin folder

---
+++ Tests/MQTTnet.Test.NugetConsumption/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MQTTnet.Test.NugetConsumption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new MqttFactory().CreateMqttServer();
+        }
+    }
+}

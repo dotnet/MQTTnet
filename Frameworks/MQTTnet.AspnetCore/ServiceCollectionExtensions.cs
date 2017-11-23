1bcf7ec29c0bdcc0dc0224300b57a03c93463014
Christian Kratky <christian.kratky@googlemail.com>
2017-11-23 22:34:16 +0100

    Fix ASP.NET Core breaking changes

--- Frameworks/MQTTnet.AspnetCore/ServiceCollectionExtensions.cs
+++ Frameworks/MQTTnet.AspnetCore/ServiceCollectionExtensions.cs
@@ -1,17 +1,23 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MQTTnet.Core.Adapter;
+using MQTTnet.Core.Diagnostics;
 using MQTTnet.Core.Server;
 
 namespace MQTTnet.AspNetCore
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddHostedMqttServer(this IServiceCollection services)
+        public static IServiceCollection AddHostedMqttServer(this IServiceCollection services, MqttServerOptions options)
         {
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            services.AddSingleton(options);
+            services.AddSingleton<IMqttNetLogger>(new MqttNetLogger());
+            services.AddSingleton<MqttHostedServer>();
             services.AddSingleton<IHostedService>(s => s.GetService<MqttHostedServer>());
             services.AddSingleton<IMqttServer>(s => s.GetService<MqttHostedServer>());
-            services.AddSingleton<MqttHostedServer>();
             
             services.AddSingleton<MqttWebSocketServerAdapter>();
             services.AddSingleton<IMqttServerAdapter>(s => s.GetService<MqttWebSocketServerAdapter>());

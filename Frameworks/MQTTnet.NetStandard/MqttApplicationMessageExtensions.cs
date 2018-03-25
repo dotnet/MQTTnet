447b2aa2c682dffe73ce8e7a4f790ad2d2b02996
Christian <christian.kratky@googlemail.com>
2018-03-25 14:39:44 +0200

    Add new extension methods.

---
+++ Frameworks/MQTTnet.NetStandard/MqttApplicationMessageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace MQTTnet
+{
+    public static class MqttApplicationMessageExtensions
+    {
+        public static string ConvertPayloadToString(this MqttApplicationMessage applicationMessage)
+        {
+            if (applicationMessage == null) throw new ArgumentNullException(nameof(applicationMessage));
+
+            if (applicationMessage.Payload == null)
+            {
+                return null;
+            }
+
+            if (applicationMessage.Payload.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            return Encoding.UTF8.GetString(applicationMessage.Payload, 0, applicationMessage.Payload.Length);
+        }
+    }
+}

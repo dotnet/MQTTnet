580a431fbf4b543f26c494ec320d9934f26061ee
BP <biapar@redconsulting.it>
2018-04-09 10:59:34 +0200

    Update Startup.cs

--- Tests/MQTTnet.TestApp.AspNetCore2/Startup.cs
+++ Tests/MQTTnet.TestApp.AspNetCore2/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using MQTTnet;
 using MQTTnet.AspNetCore;
 using MQTTnet.Server;
 

2499d2919631df6908f3358261def52635764f44
Christian <christian.kratky@googlemail.com>
2018-05-16 00:12:09 +0200

    Refactor events.

---
+++ Frameworks/MQTTnet.NetStandard/Server/MqttServerEventDispatcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MQTTnet.Server
+{
+    public class MqttServerEventDispatcher
+    {
+        public event EventHandler<MqttClientConnectedEventArgs> ClientConnected;
+        public event EventHandler<MqttClientDisconnectedEventArgs> ClientDisconnected;
+        public event EventHandler<MqttClientSubscribedTopicEventArgs> ClientSubscribedTopic;
+        public event EventHandler<MqttClientUnsubscribedTopicEventArgs> ClientUnsubscribedTopic;
+
+        public event EventHandler<MqttApplicationMessageReceivedEventArgs> ApplicationMessageReceived;
+    }
+}

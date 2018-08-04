0bf77f05406f393b4f21cb2b05c032a02c0695a1
JanEggers <jan.eggers@ferag.com>
2018-08-04 16:02:46 +0200

    added tests

---
+++ Tests/MQTTnet.AspNetCore.Tests/MqttPacketSerializerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MQTTnet.Core.Tests;
+using MQTTnet.Serializer;
+
+namespace MQTTnet.AspNetCore.Tests
+{
+    [TestClass]
+    public class MqttPacketSerializerTestsWithSpanBasedReader : MqttPacketSerializerTests
+    {
+        protected override IPacketBodyReader ReaderFactory(byte[] data)
+        {
+            var result = new SpanBasedMqttPacketBodyReader();
+            result.SetBuffer(data);
+            return result;
+        }
+    }
+}

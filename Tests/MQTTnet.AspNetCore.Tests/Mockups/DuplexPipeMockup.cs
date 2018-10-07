73d33e2200b69a6867d6a83db9aed69ca385ecc9
JanEggers <jan.eggers@ferag.com>
2018-10-07 14:40:57 +0200

    fixed #421

---
+++ Tests/MQTTnet.AspNetCore.Tests/Mockups/DuplexPipeMockup.cs
@@ -0,0 +1,16 @@
+﻿using System.IO.Pipelines;
+
+namespace MQTTnet.AspNetCore.Tests.Mockups
+{
+    public class DuplexPipeMockup : IDuplexPipe
+    {
+        PipeReader IDuplexPipe.Input => Receive.Reader;
+
+        PipeWriter IDuplexPipe.Output => Send.Writer;
+
+
+        public Pipe Receive { get; set; } = new Pipe();
+
+        public Pipe Send { get; set; } = new Pipe();
+    }
+}
---
+++ Tests/MQTTnet.AspNetCore.Tests/MqttConnectionContextTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MQTTnet.AspNetCore.Tests.Mockups;
+using MQTTnet.Exceptions;
+using MQTTnet.Serializer;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MQTTnet.AspNetCore.Tests
+{
+    [TestClass]
+    public class MqttConnectionContextTest
+    {
+        [TestMethod]
+        public async Task TestReceivePacketAsyncThrowsWhenReaderCompleted()
+        {
+            var serializer = new MqttPacketSerializer {};
+            var pipe = new DuplexPipeMockup();
+            var connection = new DefaultConnectionContext();
+            connection.Transport = pipe;
+            var ctx = new MqttConnectionContext(serializer, connection);
+
+            pipe.Receive.Writer.Complete();
+
+            await Assert.ThrowsExceptionAsync<MqttCommunicationException>(() => ctx.ReceivePacketAsync(TimeSpan.FromSeconds(1), CancellationToken.None));
+        }
+    }
+}

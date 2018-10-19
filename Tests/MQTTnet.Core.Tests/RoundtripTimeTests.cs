baba6925c51bc60c162d27d4ec3161e18a2ab88a
Christian Kratky <christian.kratky@googlemail.com>
2018-10-19 23:55:02 +0200

    Add new UnitTests.

---
+++ Tests/MQTTnet.Core.Tests/RoundtripTimeTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MQTTnet.Client;
+using MQTTnet.Server;
+
+namespace MQTTnet.Core.Tests
+{
+    [TestClass]
+    public class RoundtripTimeTests
+    {
+        [TestMethod]
+        public async Task Round_Trip_Time()
+        {
+            var factory = new MqttFactory();
+            var server = factory.CreateMqttServer();
+            var receiverClient = factory.CreateMqttClient();
+            var senderClient = factory.CreateMqttClient();
+
+            await server.StartAsync(new MqttServerOptionsBuilder().Build());
+
+            await receiverClient.ConnectAsync(new MqttClientOptionsBuilder().WithTcpServer("localhost").Build());
+            await receiverClient.SubscribeAsync("#");
+
+            await senderClient.ConnectAsync(new MqttClientOptionsBuilder().WithTcpServer("localhost").Build());
+
+            TaskCompletionSource<string> response = null;
+
+            receiverClient.ApplicationMessageReceived += (sender, args) =>
+            {
+                response?.SetResult(args.ApplicationMessage.ConvertPayloadToString());
+            };
+
+            var times = new List<TimeSpan>();
+            var stopwatch = Stopwatch.StartNew();
+
+            for (var i = 0; i < 100; i++)
+            {
+                response = new TaskCompletionSource<string>();
+                await senderClient.PublishAsync("test", DateTime.UtcNow.Ticks.ToString());
+                response.Task.GetAwaiter().GetResult();
+
+                stopwatch.Stop();
+                times.Add(stopwatch.Elapsed);
+                stopwatch.Restart();
+            }           
+        }
+    }
+}

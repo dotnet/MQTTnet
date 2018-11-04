a5caab62f83905a5ebbcac634db19d79768ed874
JanEggers <jan.eggers@ferag.com>
2018-11-04 11:39:12 +0100

    fixed parallel writes

--- Tests/MQTTnet.AspNetCore.Tests/MqttConnectionContextTest.cs
+++ Tests/MQTTnet.AspNetCore.Tests/MqttConnectionContextTest.cs
@@ -2,8 +2,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MQTTnet.AspNetCore.Tests.Mockups;
 using MQTTnet.Exceptions;
+using MQTTnet.Packets;
 using MQTTnet.Serializer;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -24,6 +26,26 @@ namespace MQTTnet.AspNetCore.Tests
             pipe.Receive.Writer.Complete();
 
             await Assert.ThrowsExceptionAsync<MqttCommunicationException>(() => ctx.ReceivePacketAsync(TimeSpan.FromSeconds(1), CancellationToken.None));
+        }
+        
+        [TestMethod]
+        public async Task TestParallelWrites()
+        {
+            var serializer = new MqttPacketSerializer { };
+            var pipe = new DuplexPipeMockup();
+            var connection = new DefaultConnectionContext();
+            connection.Transport = pipe;
+            var ctx = new MqttConnectionContext(serializer, connection);
+
+            var tasks = Enumerable.Range(1, 10).Select(_ => Task.Run(async () => 
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    await ctx.SendPacketAsync(new MqttPublishPacket(), CancellationToken.None).ConfigureAwait(false);
+                }
+            }));
+
+            await Task.WhenAll(tasks).ConfigureAwait(false);
         }
     }
 }

a8a64aa397115b54a98af34e33e5babfe78029ed
Paul Fake <pauljunkaccount@gmail.com>
2018-11-21 21:35:15 -0500

    Revert "Changes to this file no longer needed"
    This reverts commit 4ea3bed846c34751d96d9f0eea4214c2bdca7454.

---
+++ Source/MQTTnet.Extensions.ManagedClient/ManagedMqttApplicationMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MQTTnet.Extensions.ManagedClient
+{
+    public class ManagedMqttApplicationMessage : IEquatable<ManagedMqttApplicationMessage>
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public MqttApplicationMessage ApplicationMessage { get; set; }
+       
+        public bool Equals(ManagedMqttApplicationMessage other)
+        {
+            return Id.Equals(other.Id);
+        }
+    }
+}

d1c3d9b453d8879970266dc19450ec0db7d0ab96
Paul Fake <pauljunkaccount@gmail.com>
2018-11-21 21:37:01 -0500

    Removed unnecessary code

--- Source/MQTTnet.Extensions.ManagedClient/ManagedMqttApplicationMessage.cs
+++ Source/MQTTnet.Extensions.ManagedClient/ManagedMqttApplicationMessage.cs
@@ -2,15 +2,10 @@
 
 namespace MQTTnet.Extensions.ManagedClient
 {
-    public class ManagedMqttApplicationMessage : IEquatable<ManagedMqttApplicationMessage>
+    public class ManagedMqttApplicationMessage
     {
         public Guid Id { get; set; } = Guid.NewGuid();
 
         public MqttApplicationMessage ApplicationMessage { get; set; }
-       
-        public bool Equals(ManagedMqttApplicationMessage other)
-        {
-            return Id.Equals(other.Id);
-        }
     }
 }

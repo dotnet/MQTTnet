0c13e7a5044394568a921de47fd86ae10fcb2b5d
Charles Quesnel <charles.quesnel@exfo.com>
2018-12-13 12:56:47 -0500

    Add overload to allow passing custom request headers and cookies when connecting with WS

---
+++ Source/MQTTnet/Client/MqttClientOptionsBuilderWebSocketParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace MQTTnet.Client
+{
+    public class MqttClientOptionsBuilderWebSocketParameters
+    {
+        public IDictionary<string, string> RequestHeaders { get; set; }
+
+        public CookieContainer CookierContainer { get; set; }
+    }
+}

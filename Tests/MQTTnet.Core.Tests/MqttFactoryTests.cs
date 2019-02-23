3b02a5f6aab174aa8e6a9802c4703a57b86b8d38
Christian <christian.kratky@googlemail.com>
2019-02-23 10:40:17 +0100

    Fix NullRefException when cancelling the server.

--- Tests/MQTTnet.Core.Tests/MqttFactoryTests.cs
+++ Tests/MQTTnet.Core.Tests/MqttFactoryTests.cs
@@ -2,8 +2,6 @@
 using MQTTnet.Diagnostics;
 using MQTTnet.Extensions.ManagedClient;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 

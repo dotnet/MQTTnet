ad4510de2354111fc6eec746cb9adfbf88634289
Kiarash Ghiaseddin <kiarash.ghiaseddin@prodot.de>
2019-03-04 21:13:22 +0100

    WIP First draft for MQTTnetServer project * Basic support for settings * Nasty code for setting up kestrel from appsettings.json

---
+++ Source/MQTTnetServer/Settings/ListenProtocolTypes.cs
@@ -0,0 +1,23 @@
+﻿namespace MQTTnetServer.Settings
+{
+    /// <summary>
+    /// Listen Protocol Types
+    /// </summary>
+    public enum ListenProtocolTypes
+    {
+        /// <summary>
+        /// HTTP
+        /// </summary>
+        HTTP = 0,
+
+        /// <summary>
+        /// HTTPS
+        /// </summary>
+        HTTPS = 1,
+
+        /// <summary>
+        /// MQTT
+        /// </summary>
+        MQTT = 20
+    }
+}
---
+++ Source/MQTTnetServer/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MQTTnetServer
+{
+    /// <summary>
+    /// Web App Startup
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        /// <summary>
+        /// Application Settings
+        /// </summary>
+        public IConfigurationRoot Configuration { get; }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container. 
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+    }
+}

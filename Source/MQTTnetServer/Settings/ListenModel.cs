b68247f56d99d1d82ccc917ee88dd92c4d47c754
Kiarash Ghiaseddin <kiarash.ghiaseddin@prodot.de>
2019-03-04 21:13:22 +0100

    WIP First draft for MQTTnetServer project * Basic support for settings * Nasty code for setting up kestrel from appsettings.json

---
+++ Source/MQTTnetServer/Settings/ListenModel.cs
@@ -0,0 +1,30 @@
+﻿namespace MQTTnetServer.Settings
+{
+    /// <summary>
+    /// Listen Entry Settings Model
+    /// </summary>
+    public class ListenModel
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ListenModel()
+        {
+        }
+
+        /// <summary>
+        /// Listen Address
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Listen Port
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// Protocol Type
+        /// </summary>
+        public ListenProtocolTypes Protocol { get; set; } = ListenProtocolTypes.HTTP;
+    }
+}
---
+++ Source/MQTTnetServer/Settings/SettingsModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MQTTnetServer.Settings
+{
+    /// <summary>
+    /// Main Settings Model
+    /// </summary>
+    public class SettingsModel
+    {
+        /// <summary>
+        /// Listen Settings
+        /// </summary>
+        public IEnumerable<ListenModel> Listen { get; set; }
+    }
+}

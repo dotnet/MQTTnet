cf984bf2742236c83e0a15f73de17e23e7321886
JanEggers <jan.Eggers@ferag.com>
2019-04-15 15:52:51 +0200

    added benchmark option to compare netfx and core runtime

---
+++ Tests/MQTTnet.Benchmarks/Configurations/BaseConfig.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Configs;
+using System.Linq;
+
+namespace MQTTnet.Benchmarks.Configurations
+{
+    public class BaseConfig : ManualConfig
+    {
+        public BaseConfig()
+        {
+            Add(DefaultConfig.Instance.GetLoggers().ToArray()); // manual config has no loggers by default
+            Add(DefaultConfig.Instance.GetExporters().ToArray()); // manual config has no exporters by default
+            Add(DefaultConfig.Instance.GetColumnProviders().ToArray()); // manual config has no columns by default
+        }
+    }
+}

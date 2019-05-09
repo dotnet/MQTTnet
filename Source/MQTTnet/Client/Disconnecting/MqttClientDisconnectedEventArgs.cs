c5dde6a4eb1f900f714071c7e1c95eaeea14558c
Christian Kratky <christian.kratky@googlemail.com>
2019-05-09 22:08:56 +0200

    Add authentication result to disconnected handler.

--- Source/MQTTnet/Client/Disconnecting/MqttClientDisconnectedEventArgs.cs
+++ Source/MQTTnet/Client/Disconnecting/MqttClientDisconnectedEventArgs.cs
@@ -1,17 +1,21 @@
 ﻿using System;
+using MQTTnet.Client.Connecting;
 
 namespace MQTTnet.Client.Disconnecting
 {
     public class MqttClientDisconnectedEventArgs : EventArgs
     {
-        public MqttClientDisconnectedEventArgs(bool clientWasConnected, Exception exception)
+        public MqttClientDisconnectedEventArgs(bool clientWasConnected, Exception exception, MqttClientAuthenticateResult authenticateResult)
         {
             ClientWasConnected = clientWasConnected;
             Exception = exception;
+            AuthenticateResult = authenticateResult;
         }
 
         public bool ClientWasConnected { get; }
 
         public Exception Exception { get; }
+
+        public MqttClientAuthenticateResult AuthenticateResult { get; }
     }
 }

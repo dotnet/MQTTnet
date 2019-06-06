0321415671a4453907174e3705ab63e5fda6eb0c
Christian Kratky <christian.kratky@googlemail.com>
2019-06-06 19:32:00 +0200

    Execute disconnected handler in new task.

---
+++ Source/MQTTnet/Internal/TaskExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using MQTTnet.Diagnostics;
+
+namespace MQTTnet.Internal
+{
+    public static class TaskExtensions
+    {
+        public static void Forget(this Task task, IMqttNetChildLogger logger)
+        {
+            task?.ContinueWith(t =>
+                {
+                    logger.Error(t.Exception, "Unhandled exception.");
+                },
+                TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}

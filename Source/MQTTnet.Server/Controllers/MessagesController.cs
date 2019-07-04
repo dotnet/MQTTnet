019f428f0a41c83648e5e1e13d9eb52426206d2d
Christian Kratky <christian.kratky@googlemail.com>
2019-07-04 20:27:48 +0200

    MQTTnet.Server: Add HTTP API for publishing of messages.

---
+++ Source/MQTTnet.Server/Controllers/MessagesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MQTTnet.Server.Mqtt;
+
+namespace MQTTnet.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class MessagesController : Controller
+    {
+        private readonly MqttServerService _mqttServerService;
+
+        public MessagesController(MqttServerService mqttServerService)
+        {
+            _mqttServerService = mqttServerService ?? throw new ArgumentNullException(nameof(mqttServerService));
+        }
+
+        [Route("api/v1/messages")]
+        [HttpPost]
+        public async Task<ActionResult> PostMessage(MqttApplicationMessage message)
+        {
+            await _mqttServerService.PublishAsync(message);
+            return Ok();
+        }
+
+        [Route("api/v1/messages/{*topic}")]
+        [HttpPost]
+        public Task<ActionResult> PostMessage(string topic, string payload)
+        {
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic(topic)
+                .WithPayload(payload)
+                .Build();
+
+            return PostMessage(message);
+        }
+    }
+}

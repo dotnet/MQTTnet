ed06ea6bc95843a9924dc30db1e038e4e36f5035
Christian Kratky <christian.kratky@googlemail.com>
2019-07-19 19:32:33 +0200

    Add version information to MQTTnet.Server.

---
+++ Source/MQTTnet.Server/Controllers/ServerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace MQTTnet.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class ServerController : Controller
+    {
+        [Route("api/v1/server/version")]
+        [HttpGet]
+        public ActionResult<string> GetVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        }
+    }
+}

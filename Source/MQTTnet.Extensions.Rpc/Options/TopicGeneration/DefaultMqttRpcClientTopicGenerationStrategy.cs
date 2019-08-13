7a3ab6a2b955e78f1b1ab59cf14a1a6ee378949a
Christian Kratky <christian.kratky@googlemail.com>
2019-08-13 19:53:55 +0200

    Add support for different strategies when generating RPC topics.

---
+++ Source/MQTTnet.Extensions.Rpc/Options/TopicGeneration/DefaultMqttRpcClientTopicGenerationStrategy.cs
@@ -0,0 +1,20 @@
+﻿using MQTTnet.Extensions.Rpc.Options.TopicGeneration;
+using System;
+
+namespace MQTTnet.Extensions.Rpc.Options
+{
+    public class DefaultMqttRpcClientTopicGenerationStrategy : IMqttRpcClientTopicGenerationStrategy
+    {
+        public MqttRpcTopicPair CreateRpcTopics(TopicGenerationContext context)
+        {
+            var requestTopic = $"MQTTnet.RPC/{Guid.NewGuid():N}/{context.MethodName}";
+            var responseTopic = requestTopic + "/response";
+
+            return new MqttRpcTopicPair
+            {
+                RequestTopic = requestTopic,
+                ResponseTopic = responseTopic
+            };
+        }
+    }
+}

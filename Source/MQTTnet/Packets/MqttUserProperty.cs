f0d1830bf8c0f9fb75dc0ad66c22207325a1fa71
Christian Kratky <christian.kratky@googlemail.com>
2019-10-03 15:23:35 +0200

    Update docs. Add UnitTests.

--- Source/MQTTnet/Packets/MqttUserProperty.cs
+++ Source/MQTTnet/Packets/MqttUserProperty.cs
@@ -13,5 +13,31 @@ namespace MQTTnet.Packets
         public string Name { get; }
 
         public string Value { get; }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Value.GetHashCode();
+        }
+
+        public override bool Equals(object other)
+        {
+            return Equals(other as MqttUserProperty);
+        }
+
+        public bool Equals(MqttUserProperty other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(other, this))
+            {
+                return true;
+            }
+
+            return string.Equals(Name, other.Name, StringComparison.Ordinal) &&
+                string.Equals(Value, other.Value, StringComparison.Ordinal);
+        }
     }
 }

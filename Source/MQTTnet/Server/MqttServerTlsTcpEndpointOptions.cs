23c9984d49c8ddac1cede099d5d1b6743588e39c
Ben Origas <borigas@ocuvera.com>
2019-11-15 09:37:33 -0600

    Add an x509 certificate parameter

--- Source/MQTTnet/Server/MqttServerTlsTcpEndpointOptions.cs
+++ Source/MQTTnet/Server/MqttServerTlsTcpEndpointOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Security;
 using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MQTTnet.Server
 {
@@ -11,6 +12,8 @@ namespace MQTTnet.Server
         }
 
         public byte[] Certificate { get; set; }
+
+        public X509Certificate2 X509Certificate { get; set; }
 
         public IMqttServerCertificateCredentials CertificateCredentials { get; set; }
 

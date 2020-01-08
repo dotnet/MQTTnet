c64d2a2ef9973e1380dde53ee35079a97f6b8fd2
Jim Schaad <ietf@augustcellars.com>
2020-01-08 11:53:00 -0800

    Update formatting per request
    Update the formatting to match the review

--- Source/MQTTnet/Server/MqttSubscriptionInterceptorContext.cs
+++ Source/MQTTnet/Server/MqttSubscriptionInterceptorContext.cs
@@ -5,12 +5,15 @@ namespace MQTTnet.Server
 {
     public class MqttSubscriptionInterceptorContext
     {
+        private bool _acceptSubscription = true;
+        private MqttSubscribeReasonCode _resultCode;
+
         public MqttSubscriptionInterceptorContext(string clientId, TopicFilter topicFilter, IDictionary<object, object> sessionItems)
         {
             ClientId = clientId;
             TopicFilter = topicFilter;
             SessionItems = sessionItems;
-            resultCode = ConvertToSubscribeReasonCode(topicFilter.QualityOfServiceLevel);
+            _resultCode = ConvertToSubscribeReasonCode(topicFilter.QualityOfServiceLevel);
         }
 
         public string ClientId { get; }
@@ -22,23 +25,27 @@ namespace MQTTnet.Server
         /// </summary>
         public IDictionary<object, object> SessionItems { get; }
 
-        private bool acceptSubscription = true;
         public bool AcceptSubscription
         {
-            get => acceptSubscription;
+            get => _acceptSubscription;
             set {
-                if (!value && resultCode < MqttSubscribeReasonCode.UnspecifiedError) resultCode = MqttSubscribeReasonCode.UnspecifiedError;
-                acceptSubscription = value;
+                if (!value && _resultCode < MqttSubscribeReasonCode.UnspecifiedError)
+                {
+                    _resultCode = MqttSubscribeReasonCode.UnspecifiedError;
+                }
+                _acceptSubscription = value;
             }
         }
 
-        private MqttSubscribeReasonCode resultCode;
         public MqttSubscribeReasonCode ResultCode
         {
-            get => resultCode;
+            get => _resultCode;
             set {
-                if (AcceptSubscription && value >= MqttSubscribeReasonCode.UnspecifiedError) AcceptSubscription = false;
-                resultCode = value;
+                if (AcceptSubscription && value >= MqttSubscribeReasonCode.UnspecifiedError)
+                {
+                    AcceptSubscription = false;
+                }
+                _resultCode = value;
             }
         }
 
@@ -48,10 +55,10 @@ namespace MQTTnet.Server
         {
             switch (qualityOfServiceLevel)
             {
-            case MqttQualityOfServiceLevel.AtMostOnce: return MqttSubscribeReasonCode.GrantedQoS0;
-            case MqttQualityOfServiceLevel.AtLeastOnce: return MqttSubscribeReasonCode.GrantedQoS1;
-            case MqttQualityOfServiceLevel.ExactlyOnce: return MqttSubscribeReasonCode.GrantedQoS2;
-            default: return MqttSubscribeReasonCode.UnspecifiedError;
+                case MqttQualityOfServiceLevel.AtMostOnce: return MqttSubscribeReasonCode.GrantedQoS0;
+                case MqttQualityOfServiceLevel.AtLeastOnce: return MqttSubscribeReasonCode.GrantedQoS1;
+                case MqttQualityOfServiceLevel.ExactlyOnce: return MqttSubscribeReasonCode.GrantedQoS2;
+                default: return MqttSubscribeReasonCode.UnspecifiedError;
             }
         }
     }

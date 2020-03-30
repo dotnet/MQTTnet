04c441f304c9929dce67dfcf466bab5c5f69120f
PMExtra <pm@jubeat.net>
2020-03-30 17:10:04 +0800

    GetUserProperty extension.

---
+++ Tests/MQTTnet.Core.Tests/MqttApplicationMessage_Tests.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MQTTnet.Extensions;
+using MQTTnet.Packets;
+
+namespace MQTTnet.Tests
+{
+    [TestClass]
+    public class MqttApplicationMessage_Tests
+    {
+        [TestMethod]
+        public void GetUserProperty_Test()
+        {
+            var message = new MqttApplicationMessage
+            {
+                UserProperties = new List<MqttUserProperty>
+                {
+                    new MqttUserProperty("foo", "bar"),
+                    new MqttUserProperty("value", "1011"),
+                    new MqttUserProperty("CASE", "insensitive")
+                }
+            };
+
+            Assert.AreEqual("bar", message.GetUserProperty("foo"));
+            Assert.AreEqual(1011, message.GetUserProperty<int>("value"));
+            Assert.AreEqual("insensitive", message.GetUserProperty("case"));
+        }
+    }
+}

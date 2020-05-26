0a4be62e31f6abf8b2c91b79c503d018e681fd25
Christian Kratky <christian.kratky@googlemail.com>
2020-05-26 23:46:43 +0200

    Add abstraction layer for server certificate.

---
+++ Source/MQTTnet/Certificates/ICertificateProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MQTTnet.Certificates
+{
+    public interface ICertificateProvider
+    {
+        X509Certificate2 GetCertificate();
+    }
+}

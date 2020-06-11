0a76ac15c64d085a972a545118f0a0d1ad14efa7
SeppPenner <HansM2013@gmx.de>
2020-06-11 18:06:24 +0200

    Added missing disconnect reason codes.

--- Source/MQTTnet/Client/Disconnecting/MqttClientDisconnectReason.cs
+++ Source/MQTTnet/Client/Disconnecting/MqttClientDisconnectReason.cs
@@ -3,6 +3,34 @@
     public enum MqttClientDisconnectReason
     {
         NormalDisconnection = 0,
-
+        DisconnectWithWillMessage = 4,
+        UnspecifiedError = 128,
+        MalformedPacket = 129,
+        ProtocolError = 130,
+        ImplementationSpecificError = 131,
+        NotAuthorized = 135,
+        ServerBusy = 137,
+        ServerShuttingDown = 139,
+        BadAuthenticationMethod = 140,
+        KeepaliveTimeout = 141,
+        SessionTakenOver = 142,
+        TopicFilterInvalid = 143,
+        TopicNameInvalid = 144,
+        ReceiveMaximumExceeded = 147,
+        TopicAliasInvalid = 148,
+        PacketTooLarge = 149,
+        MessageRateTooHigh = 150,
+        QuotaExceeded = 151,
+        AdministrativeAction = 152,
+        PayloadFormatInvalid = 153,
+        RetainNotSupported = 154,
+        QosNotSupported = 155,
+        UseAnotherServer = 156,
+        ServerMoved = 157,
+        SharedSubscriptionsNotSupported = 158,
+        ConnectionRateExceeded = 159,
+        MaximumConnectTime = 160,
+        SubscriptionIdentifiersNotSupported = 161,
+        WildcardSubscriptionsNotSupported = 162
     }
 }

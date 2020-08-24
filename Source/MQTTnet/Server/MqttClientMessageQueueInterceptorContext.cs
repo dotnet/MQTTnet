fd789417e769b2e6d7cd8cb457d5aa5ca3c65803
Marc Sallin <marc.sallin@outlook.com>
2020-08-25 00:50:30 +0200

    Extend MqttClientMessageQueueInterceptorContext to expose SubscriptionQualityOfServiceLevel

--- Source/MQTTnet/Server/MqttClientMessageQueueInterceptorContext.cs
+++ Source/MQTTnet/Server/MqttClientMessageQueueInterceptorContext.cs
@@ -1,12 +1,15 @@
-﻿namespace MQTTnet.Server
+﻿using MQTTnet.Protocol;
+
+namespace MQTTnet.Server
 {
     public class MqttClientMessageQueueInterceptorContext
     {
-        public MqttClientMessageQueueInterceptorContext(string senderClientId, string receiverClientId, MqttApplicationMessage applicationMessage)
+        public MqttClientMessageQueueInterceptorContext(string senderClientId, string receiverClientId, MqttApplicationMessage applicationMessage, MqttQualityOfServiceLevel subscriptionQualityOfServiceLevel)
         {
             SenderClientId = senderClientId;
             ReceiverClientId = receiverClientId;
             ApplicationMessage = applicationMessage;
+            SubscriptionQualityOfServiceLevel = subscriptionQualityOfServiceLevel;
         }
 
         public string SenderClientId { get; }
@@ -16,5 +19,7 @@
         public MqttApplicationMessage ApplicationMessage { get; set; }
 
         public bool AcceptEnqueue { get; set; } = true;
+
+        public MqttQualityOfServiceLevel SubscriptionQualityOfServiceLevel { get; set; }
     }
 }

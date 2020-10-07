74fa86cf799a9b356913bbce248e9dbeb416b62f
Christian Kratky <christian.kratky@googlemail.com>
2020-10-07 19:38:42 +0200

    ManagedClient: Expose internal client.

--- Source/MQTTnet.Extensions.ManagedClient/IManagedMqttClient.cs
+++ Source/MQTTnet.Extensions.ManagedClient/IManagedMqttClient.cs
@@ -4,30 +4,46 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using MQTTnet.Client;
 
 namespace MQTTnet.Extensions.ManagedClient
 {
     public interface IManagedMqttClient : IApplicationMessageReceiver, IApplicationMessagePublisher, IDisposable
     {
-        bool IsStarted { get; }
-        bool IsConnected { get; }
-        int PendingApplicationMessagesCount { get; }
-        IManagedMqttClientOptions Options { get; }
+        /// <summary>
+        /// Gets the internally used MQTT client.
+        /// This property should be used with caution because manipulating the internal client might break the managed client.
+        /// </summary>
+        IMqttClient InternalClient { get; }
 
+        bool IsStarted { get; }
+        
+        bool IsConnected { get; }
+        
+        int PendingApplicationMessagesCount { get; }
+        
+        IManagedMqttClientOptions Options { get; }
+        
         IMqttClientConnectedHandler ConnectedHandler { get; set; }
+        
         IMqttClientDisconnectedHandler DisconnectedHandler { get; set; }
 
         IApplicationMessageProcessedHandler ApplicationMessageProcessedHandler { get; set; }
+        
         IApplicationMessageSkippedHandler ApplicationMessageSkippedHandler { get; set; }
 
         IConnectingFailedHandler ConnectingFailedHandler { get; set; }
+        
         ISynchronizingSubscriptionsFailedHandler SynchronizingSubscriptionsFailedHandler { get; set; }
 
         Task StartAsync(IManagedMqttClientOptions options);
+        
         Task StopAsync();
+        
         Task PingAsync(CancellationToken cancellationToken);
 
         Task SubscribeAsync(IEnumerable<MqttTopicFilter> topicFilters);
+        
         Task UnsubscribeAsync(IEnumerable<string> topics);
 
         Task PublishAsync(ManagedMqttApplicationMessage applicationMessages);

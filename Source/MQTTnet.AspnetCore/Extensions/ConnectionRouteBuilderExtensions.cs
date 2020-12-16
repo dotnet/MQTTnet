02e65bca67e460888336bcba9aa8462f20def2c3
JanEggers <jan.Eggers@ferag.com>
2020-12-16 14:41:54 +0100

    fixed .net 5 build

--- Source/MQTTnet.AspnetCore/Extensions/ConnectionRouteBuilderExtensions.cs
+++ Source/MQTTnet.AspnetCore/Extensions/ConnectionRouteBuilderExtensions.cs
@@ -12,6 +12,7 @@ namespace MQTTnet.AspNetCore.Extensions
 #if NETCOREAPP3_1
         [Obsolete("This class is obsolete and will be removed in a future version. The recommended alternative is to use MapMqtt inside Microsoft.AspNetCore.Builder.UseEndpoints(...).")]
 #endif
+#if NETCOREAPP3_1 || NETCOREAPP2_1 || NETSTANDARD
         public static void MapMqtt(this ConnectionsRouteBuilder connection, PathString path)
         {
             connection.MapConnectionHandler<MqttConnectionHandler>(path, options =>
@@ -19,5 +20,6 @@ namespace MQTTnet.AspNetCore.Extensions
                 options.WebSockets.SubProtocolSelector = MqttSubProtocolSelector.SelectSubProtocol;
             });
         }
+#endif
     }
 }
--- Source/MQTTnet.AspnetCore/Extensions/EndpointRouterExtensions.cs
+++ Source/MQTTnet.AspnetCore/Extensions/EndpointRouterExtensions.cs
@@ -1,5 +1,5 @@
 ﻿
-#if NETCOREAPP3_1
+#if NETCOREAPP3_1 || NET5_0
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;

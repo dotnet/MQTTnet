fe0a05f666532e57981b14dd6198b9f6bb819a74
SeppPenner <HansM2013@gmx.de>
2021-01-15 20:43:57 +0100

    Hopefully completely reset the IManagedMqttClient file.

--- Source/MQTTnet.Extensions.ManagedClient/IManagedMqttClient.cs
+++ Source/MQTTnet.Extensions.ManagedClient/IManagedMqttClient.cs
@@ -17,33 +17,33 @@ namespace MQTTnet.Extensions.ManagedClient
         IMqttClient InternalClient { get; }
 
         bool IsStarted { get; }
-
+        
         bool IsConnected { get; }
-
+        
         int PendingApplicationMessagesCount { get; }
-
+        
         IManagedMqttClientOptions Options { get; }
-
+        
         IMqttClientConnectedHandler ConnectedHandler { get; set; }
-
+        
         IMqttClientDisconnectedHandler DisconnectedHandler { get; set; }
 
         IApplicationMessageProcessedHandler ApplicationMessageProcessedHandler { get; set; }
-
+        
         IApplicationMessageSkippedHandler ApplicationMessageSkippedHandler { get; set; }
 
         IConnectingFailedHandler ConnectingFailedHandler { get; set; }
-
+        
         ISynchronizingSubscriptionsFailedHandler SynchronizingSubscriptionsFailedHandler { get; set; }
 
         Task StartAsync(IManagedMqttClientOptions options);
-
+        
         Task StopAsync();
-
+        
         Task PingAsync(CancellationToken cancellationToken);
 
         Task SubscribeAsync(IEnumerable<MqttTopicFilter> topicFilters);
-
+        
         Task UnsubscribeAsync(IEnumerable<string> topics);
 
         Task PublishAsync(ManagedMqttApplicationMessage applicationMessages);

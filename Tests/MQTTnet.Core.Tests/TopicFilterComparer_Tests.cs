a8994a2d1c7f187c71b2d17f7a3fb2bde5b95d93
Christian <christian.kratky@googlemail.com>
2021-01-29 23:17:16 +0100

    Add Unit Tests.

--- Tests/MQTTnet.Core.Tests/TopicFilterComparer_Tests.cs
+++ Tests/MQTTnet.Core.Tests/TopicFilterComparer_Tests.cs
@@ -7,6 +7,20 @@ namespace MQTTnet.Tests
     public class TopicFilterComparer_Tests
     {
         [TestMethod]
+        public void TopicFilterComparer_Plus_Match_With_Separator_Only()
+        {
+            //A Topic Name or Topic Filter consisting only of the ‘/’ character is valid
+            CompareAndAssert("A", "+", true);
+        }
+
+        [TestMethod]
+        public void TopicFilterComparer_Hash_Match_With_Separator_Only()
+        {
+            //A Topic Name or Topic Filter consisting only of the ‘/’ character is valid
+            CompareAndAssert("/", "#", true);
+        }
+
+        [TestMethod]
         public void TopicFilterComparer_DirectMatch()
         {
             CompareAndAssert("A/B/C", "A/B/C", true);

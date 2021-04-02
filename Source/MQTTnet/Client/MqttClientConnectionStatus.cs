986b152cc30cf47780d01956c1e0ae6bec4a0a09
SilverFox <yyjdelete@126.com>
2021-04-02 22:38:20 +0800

    Rename MqttClient.ConnectState to MqttClientConnectionStatus

---
+++ Source/MQTTnet/Client/MqttClientConnectionStatus.cs
@@ -0,0 +1,11 @@
+namespace MQTTnet.Client
+{
+    public enum MqttClientConnectionStatus
+
+    {
+        Disconnected = 0,
+        Disconnecting,
+        Connected,
+        Connecting
+    }
+}

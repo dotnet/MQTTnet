3cdf6d126150479f2f806dd7ffe1a90d0c117871
HansM <HansM2013@gmx.de>
2021-09-05 11:42:42 +0200

    [MQTTnet, MQTTnet.Extensions.ManagedClient] Fixed bug that allowed invalid subscriptions (Thanks to @marcelwinh). (#1226)

---
+++ Tests/MQTTnet.Core.Tests/MqttTopicValidatorSubscribe_Tests.cs
@@ -0,0 +1,54 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MQTTnet.Exceptions;
+using MQTTnet.Protocol;
+
+namespace MQTTnet.Tests
+{
+    [TestClass]
+    public class MqttTopicValidatorSubscribe_Tests
+    {
+        [TestMethod]
+        public void Valid_Topic()
+        {
+            MqttTopicValidator.ThrowIfInvalidSubscribe("/a/b/c");
+        }
+
+        [TestMethod]
+        public void Valid_Topic_Plus_In_Between()
+        {
+            MqttTopicValidator.ThrowIfInvalidSubscribe("/a/+/c");
+        }
+
+        [TestMethod]
+        public void Valid_Topic_Plus_Last_Char()
+        {
+            MqttTopicValidator.ThrowIfInvalidSubscribe("/a/+");
+        }
+
+        [TestMethod]
+        public void Valid_Topic_Hash_Last_Char()
+        {
+            MqttTopicValidator.ThrowIfInvalidSubscribe("/a/#");
+        }
+
+        [TestMethod]
+        public void Valid_Topic_Only_Hash()
+        {
+            MqttTopicValidator.ThrowIfInvalidSubscribe("#");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MqttProtocolViolationException))]
+        public void Invalid_Topic_Hash_In_Between()
+        {
+            MqttTopicValidator.ThrowIfInvalidSubscribe("/a/#/c");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MqttProtocolViolationException))]
+        public void Invalid_Topic_Empty()
+        {
+            MqttTopicValidator.ThrowIfInvalidSubscribe(string.Empty);
+        }
+    }
+}

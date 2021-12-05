2ca838cbfab578977ea6777668ffa0c434156d13
Christian <christian.kratky@googlemail.com>
2021-12-05 13:45:25 +0100

    Fix build scripts.

--- Build/build.ps1
+++ Build/build.ps1
@@ -14,13 +14,15 @@ Write-Host
 
 # Cleanup
 Get-ChildItem -Path ".\..\" -Filter "*.nupkg" -Recurse | Remove-Item
+Get-ChildItem -Path ".\..\" -Filter "*.snupkg" -Recurse | Remove-Item
 
 # Build and execute tests
 &$msbuild ..\Tests\MQTTnet.Core.Tests\MQTTnet.Tests.csproj /t:Clean /t:Restore /t:Build /p:Configuration="Release" /p:TargetFramework="net5.0" /verbosity:m
 &$msbuild ..\Tests\MQTTnet.AspNetCore.Tests\MQTTnet.AspNetCore.Tests.csproj /t:Clean /t:Restore /t:Build /p:Configuration="Release" /p:TargetFramework="net5.0" /verbosity:m
 
-#vstest.console.exe ..\Tests\MQTTnet.Core.Tests\bin\Release\net5.0\MQTTnet.Tests.dll
-#vstest.console.exe ..\Tests\MQTTnet.AspNetCore.Tests\bin\Release\net5.0\MQTTnet.AspNetCore.Tests.dll
+vstest.console.exe ..\Tests\MQTTnet.Core.Tests\bin\Release\net5.0\MQTTnet.Tests.dll
+vstest.console.exe ..\Tests\MQTTnet.AspNetCore.Tests\bin\Release\net5.0\MQTTnet.AspNetCore.Tests.dll
+
 $certificate = ".\..\..\Build\codeSigningKey.pfx"
 
 # Build the core library

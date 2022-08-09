3475d4d267264941aad0213b435e60fdb493281a
Schlotter, Christian <christian.schlotter@zeiss.com>
2022-08-09 18:21:15 +0200

    Only set PacketIdentifier if publishPacket is not null.

--- Source/MQTTnet/Client/Receiving/MqttApplicationMessageReceivedEventArgs.cs
+++ Source/MQTTnet/Client/Receiving/MqttApplicationMessageReceivedEventArgs.cs
@@ -13,11 +13,11 @@ namespace MQTTnet.Client
     public sealed class MqttApplicationMessageReceivedEventArgs : EventArgs
     {
         readonly Func<MqttApplicationMessageReceivedEventArgs, CancellationToken, Task> _acknowledgeHandler;
-        
+
         int _isAcknowledged;
-        
+
         public MqttApplicationMessageReceivedEventArgs(
-            string clientId, 
+            string clientId,
             MqttApplicationMessage applicationMessage,
             MqttPublishPacket publishPacket,
             Func<MqttApplicationMessageReceivedEventArgs, CancellationToken, Task> acknowledgeHandler)
@@ -25,12 +25,12 @@ namespace MQTTnet.Client
             ClientId = clientId;
             ApplicationMessage = applicationMessage ?? throw new ArgumentNullException(nameof(applicationMessage));
             PublishPacket = publishPacket;
-            PacketIdentifier = publishPacket.PacketIdentifier;
+            PacketIdentifier = publishPacket?.PacketIdentifier ?? 0;
             _acknowledgeHandler = acknowledgeHandler;
         }
 
         internal MqttPublishPacket PublishPacket { get; set; }
-        
+
         /// <summary>
         /// Gets the client identifier.
         /// Hint: This identifier needs to be unique over all used clients / devices on the broker to avoid connection issues.
@@ -55,7 +55,7 @@ namespace MQTTnet.Client
         /// Gets or sets the reason string which will be sent to the server in the ACK packet.
         /// </summary>
         public string ResponseReasonString { get; set; }
-        
+
         /// <summary>
         /// Gets or sets whether this message was handled.
         /// This value can be used in user code for custom control flow.
@@ -66,7 +66,7 @@ namespace MQTTnet.Client
         /// Gets or sets whether the library should send MQTT ACK packets automatically if required.
         /// </summary>
         public bool AutoAcknowledge { get; set; } = true;
-        
+
         public object Tag { get; set; }
 
         /// <summary>
@@ -74,18 +74,18 @@ namespace MQTTnet.Client
         /// </summary>
         public ushort PacketIdentifier { get; }
 
-        public Task AcknowledgeAsync(CancellationToken cancellationToken) 
+        public Task AcknowledgeAsync(CancellationToken cancellationToken)
         {
             if (_acknowledgeHandler == null)
             {
                 throw new NotSupportedException("Deferred acknowledgement of application message is not yet supported in MQTTnet server.");
             }
-            
+
             if (Interlocked.CompareExchange(ref _isAcknowledged, 1, 0) == 0)
             {
                 return _acknowledgeHandler(this, cancellationToken);
             }
-            
+
             throw new InvalidOperationException("The application message is already acknowledged.");
         }
     }

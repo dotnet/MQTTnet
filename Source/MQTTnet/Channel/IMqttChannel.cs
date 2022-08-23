4391179092dbb520a345b1c6a1c5b67580c4cf2f
Christian <6939810+chkr1011@users.noreply.github.com>
2022-08-23 20:46:47 +0200

    Add end of packet information to mqtt channel.

--- Source/MQTTnet/Channel/IMqttChannel.cs
+++ Source/MQTTnet/Channel/IMqttChannel.cs
@@ -12,13 +12,17 @@ namespace MQTTnet.Channel
     public interface IMqttChannel : IDisposable
     {
         string Endpoint { get; }
+        
         bool IsSecureConnection { get; }
+        
         X509Certificate2 ClientCertificate { get; }
 
         Task ConnectAsync(CancellationToken cancellationToken);
+        
         Task DisconnectAsync(CancellationToken cancellationToken);
 
         Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken);
-        Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken);
+        
+        Task WriteAsync(ArraySegment<byte> buffer, bool isEndOfPacket, CancellationToken cancellationToken);
     }
 }

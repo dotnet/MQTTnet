58061e35466c7f41f0d61b9e8f1a66233545fde9
Christian <6939810+chkr1011@users.noreply.github.com>
2022-10-19 20:23:46 +0200

    Revert "Update AsyncLock_Tests.cs"
    This reverts commit b2300095cf1e135cae638763a6965ed87034a4ef.

--- Source/MQTTnet.Tests/Internal/AsyncLock_Tests.cs
+++ Source/MQTTnet.Tests/Internal/AsyncLock_Tests.cs
@@ -43,50 +43,50 @@ namespace MQTTnet.Tests.Internal
             Assert.AreEqual(ThreadsCount, globalI);
         }
 
-//         [TestMethod]
-//         public void Lock_10_Parallel_Tasks_With_Dispose_Doesnt_Lockup()
-//         {
-//             const int ThreadsCount = 10;
-//
-//             var threads = new Task[ThreadsCount];
-//             var @lock = new AsyncLock();
-//             var globalI = 0;
-//             for (var i = 0; i < ThreadsCount; i++)
-//             {
-// #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-//                 threads[i] = Task.Run(
-//                         async () =>
-//                         {
-//                             using (await @lock.WaitAsync(CancellationToken.None))
-//                             {
-//                                 var localI = globalI;
-//                                 await Task.Delay(10); // Increase the chance for wrong data.
-//                                 localI++;
-//                                 globalI = localI;
-//                             }
-//                         })
-//                     .ContinueWith(
-//                         x =>
-//                         {
-//                             if (globalI == 5)
-//                             {
-//                                 @lock.Dispose();
-//                                 @lock = new AsyncLock();
-//                             }
-//
-//                             if (x.Exception != null)
-//                             {
-//                                 Debug.WriteLine(x.Exception.GetBaseException().GetType().Name);
-//                             }
-//                         });
-// #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-//             }
-//
-//             Task.WaitAll(threads);
-//
-//             // Expect only 6 because the others are failing due to disposal (if (globalI == 5)).
-//             Assert.AreEqual(6, globalI);
-//         }
+        [TestMethod]
+        public void Lock_10_Parallel_Tasks_With_Dispose_Doesnt_Lockup()
+        {
+            const int ThreadsCount = 10;
+
+            var threads = new Task[ThreadsCount];
+            var @lock = new AsyncLock();
+            var globalI = 0;
+            for (var i = 0; i < ThreadsCount; i++)
+            {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                threads[i] = Task.Run(
+                        async () =>
+                        {
+                            using (await @lock.WaitAsync(CancellationToken.None))
+                            {
+                                var localI = globalI;
+                                await Task.Delay(10); // Increase the chance for wrong data.
+                                localI++;
+                                globalI = localI;
+                            }
+                        })
+                    .ContinueWith(
+                        x =>
+                        {
+                            if (globalI == 5)
+                            {
+                                @lock.Dispose();
+                                @lock = new AsyncLock();
+                            }
+
+                            if (x.Exception != null)
+                            {
+                                Debug.WriteLine(x.Exception.GetBaseException().GetType().Name);
+                            }
+                        });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            }
+
+            Task.WaitAll(threads);
+
+            // Expect only 6 because the others are failing due to disposal (if (globalI == 5)).
+            Assert.AreEqual(6, globalI);
+        }
 
         [TestMethod]
         public async Task Lock_Serial_Calls()
@@ -120,32 +120,32 @@ namespace MQTTnet.Tests.Internal
             }
         }
 
-        // [TestMethod]
-        // public async Task Test_Cancellation_With_Later_Access()
-        // {
-        //     var asyncLock = new AsyncLock();
-        //
-        //     var releaser = await asyncLock.WaitAsync(CancellationToken.None).ConfigureAwait(false);
-        //
-        //     try
-        //     {
-        //         using (var timeout = new CancellationTokenSource(TimeSpan.FromSeconds(3)))
-        //         {
-        //             await asyncLock.WaitAsync(timeout.Token).ConfigureAwait(false);
-        //         }
-        //         
-        //         Assert.Fail("Exception should be thrown!");
-        //     }
-        //     catch (OperationCanceledException)
-        //     {
-        //     }
-        //
-        //     releaser.Dispose();
-        //
-        //     using (await asyncLock.WaitAsync(CancellationToken.None).ConfigureAwait(false))
-        //     {
-        //         // When the method finished, the thread got access.
-        //     }
-        // }
+        [TestMethod]
+        public async Task Test_Cancellation_With_Later_Access()
+        {
+            var asyncLock = new AsyncLock();
+
+            var releaser = await asyncLock.WaitAsync(CancellationToken.None).ConfigureAwait(false);
+
+            try
+            {
+                using (var timeout = new CancellationTokenSource(TimeSpan.FromSeconds(3)))
+                {
+                    await asyncLock.WaitAsync(timeout.Token).ConfigureAwait(false);
+                }
+                
+                Assert.Fail("Exception should be thrown!");
+            }
+            catch (OperationCanceledException)
+            {
+            }
+
+            releaser.Dispose();
+
+            using (await asyncLock.WaitAsync(CancellationToken.None).ConfigureAwait(false))
+            {
+                // When the method finished, the thread got access.
+            }
+        }
     }
 }

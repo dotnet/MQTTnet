26b52773e1d9f2f154eeab4ac25d8d23d0eb1412
Christian <6939810+chkr1011@users.noreply.github.com>
2022-11-11 08:09:58 +0100

    Revert race condition prone changes

--- Source/MQTTnet/Internal/MqttPacketBusItem.cs
+++ Source/MQTTnet/Internal/MqttPacketBusItem.cs
@@ -10,11 +10,8 @@ namespace MQTTnet.Internal
 {
     public sealed class MqttPacketBusItem
     {
-        AsyncTaskCompletionSource<bool> _promise;
-        Exception _exception;
-        bool _isCanceled;
-        bool _isCompleted;
-
+        readonly AsyncTaskCompletionSource<bool> _promise = new AsyncTaskCompletionSource<bool>();
+        
         public MqttPacketBusItem(MqttPacket packet)
         {
             Packet = packet ?? throw new ArgumentNullException(nameof(packet));
@@ -26,66 +23,22 @@ namespace MQTTnet.Internal
 
         public void Cancel()
         {
-            if (_isCanceled)
-            {
-                throw new InvalidOperationException("The packet bus item is already canceled.");
-            }
-
-            _isCanceled = true;
-            _promise?.TrySetCanceled();
+            _promise.TrySetCanceled();
         }
 
         public void Complete()
         {
-            if (_isCompleted)
-            {
-                throw new InvalidOperationException("The packet bus item is already delivered.");
-            }
-
-            _isCompleted = true;
-
-            _promise?.TrySetResult(true);
+            _promise.TrySetResult(true);
             Completed?.Invoke(this, EventArgs.Empty);
         }
 
         public void Fail(Exception exception)
         {
-            if (_exception != null)
-            {
-                throw new InvalidOperationException("The packet bus item is already failed.");
-            }
-
-            _exception = exception ?? throw new ArgumentNullException(nameof(exception));
-
-            _promise?.TrySetException(exception);
+            _promise.TrySetException(exception);
         }
 
         public Task WaitAsync()
         {
-            // The task is being created on demand. The intention is to avoid allocations without any use.
-            // This results in the fact that the operation may already be completed / failed / canceled
-            // when the task is being created.
-
-            if (_promise == null)
-            {
-                _promise = new AsyncTaskCompletionSource<bool>();
-
-                if (_exception != null)
-                {
-                    _promise.TrySetException(_exception);
-                }
-
-                if (_isCanceled)
-                {
-                    _promise.TrySetCanceled();
-                }
-
-                if (_isCompleted)
-                {
-                    _promise.TrySetResult(true);
-                }
-            }
-
             return _promise.Task;
         }
     }

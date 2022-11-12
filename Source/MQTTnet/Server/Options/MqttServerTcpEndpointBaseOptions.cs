26e817dc00b8054d65ae153309ceabafeb5ecbc5
Christian <6939810+chkr1011@users.noreply.github.com>
2022-11-12 13:23:01 +0100

    Expose TCP keep alive options to options builder

--- Source/MQTTnet/Server/Options/MqttServerTcpEndpointBaseOptions.cs
+++ Source/MQTTnet/Server/Options/MqttServerTcpEndpointBaseOptions.cs
@@ -17,6 +17,30 @@ namespace MQTTnet.Server
 
         public bool NoDelay { get; set; } = true;
 
+        /// <summary>
+        ///     Gets or sets whether the sockets keep alive feature should be used.
+        ///     The value _null_ indicates that the OS and framework defaults should be used.
+        /// </summary>
+        public bool? KeepAlive { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the TCP keep alive interval.
+        ///     The value _null_ indicates that the OS and framework defaults should be used.
+        /// </summary>
+        public int? TcpKeepAliveInterval { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the TCP keep alive retry count.
+        ///     The value _null_ indicates that the OS and framework defaults should be used.
+        /// </summary>
+        public int? TcpKeepAliveRetryCount { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the TCP keep alive time.
+        ///     The value _null_ indicates that the OS and framework defaults should be used.
+        /// </summary>
+        public int? TcpKeepAliveTime { get; set; }
+
         public LingerOption LingerState { get; set; } = new LingerOption(true, 0);
 
 #if WINDOWS_UWP

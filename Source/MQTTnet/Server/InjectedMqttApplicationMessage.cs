bda4ab7025fc12efd719b1fed686d3d37cedf96b
老九 <366193849@qq.com>
2022-11-26 21:50:08 +0800

    Set string.Empty as default value for SenderClientId (#1583)

--- Source/MQTTnet/Server/InjectedMqttApplicationMessage.cs
+++ Source/MQTTnet/Server/InjectedMqttApplicationMessage.cs
@@ -13,8 +13,8 @@ namespace MQTTnet.Server
         {
             ApplicationMessage = applicationMessage ?? throw new ArgumentNullException(nameof(applicationMessage));
         }
-        
-        public string SenderClientId { get; set; }
+
+        public string SenderClientId { get; set; } = string.Empty;
 
         public MqttApplicationMessage ApplicationMessage { get; set; }
     }

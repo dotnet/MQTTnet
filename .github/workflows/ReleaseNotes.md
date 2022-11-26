32cc1ff7b151dd1b5800b91fcd158c1de61aaec9
Christian <6939810+chkr1011@users.noreply.github.com>
2022-11-26 14:49:28 +0100

    Refactor code and update release notes

--- .github/workflows/ReleaseNotes.md
+++ .github/workflows/ReleaseNotes.md
@@ -1,1 +1,2 @@
 [Core] Fixed a memory leak in _AsyncSignal_ implementation (#1586, thanks to @mario-zelger).
+[Server] Fix not properly reset statistics (#1587, thanks to @damikun).
--- Source/MQTTnet/Server/Internal/MqttClientStatistics.cs
+++ Source/MQTTnet/Server/Internal/MqttClientStatistics.cs
@@ -10,14 +10,14 @@ namespace MQTTnet.Server
 {
     public sealed class MqttClientStatistics
     {
+        long _receivedApplicationMessagesCount;
+
         // Start with 1 because the CONNACK packet is not counted here.
         long _receivedPacketsCount = 1;
+        long _sentApplicationMessagesCount;
 
         // Start with 1 because the CONNECT packet is not counted here.
         long _sentPacketsCount = 1;
-
-        long _receivedApplicationMessagesCount;
-        long _sentApplicationMessagesCount;
 
         public MqttClientStatistics()
         {
@@ -28,36 +28,36 @@ namespace MQTTnet.Server
 
             LastNonKeepAlivePacketReceivedTimestamp = ConnectedTimestamp;
         }
-        
+
         public DateTime ConnectedTimestamp { get; }
 
-        /// <summary>
-        /// Timestamp of the last package that has been sent to the client ("received" from the client's perspective)
-        /// </summary>
-        public DateTime LastPacketReceivedTimestamp { get; private set; }
-                
-        /// <summary>
-        /// Timestamp of the last package that has been received from the client ("sent" from the client's perspective)
-        /// </summary>
-        public DateTime LastPacketSentTimestamp { get; private set; }
-        
         public DateTime LastNonKeepAlivePacketReceivedTimestamp { get; private set; }
 
-        public long SentApplicationMessagesCount => Interlocked.Read(ref _sentApplicationMessagesCount);
-        
+        /// <summary>
+        ///     Timestamp of the last package that has been sent to the client ("received" from the client's perspective)
+        /// </summary>
+        public DateTime LastPacketReceivedTimestamp { get; private set; }
+
+        /// <summary>
+        ///     Timestamp of the last package that has been received from the client ("sent" from the client's perspective)
+        /// </summary>
+        public DateTime LastPacketSentTimestamp { get; private set; }
+
         public long ReceivedApplicationMessagesCount => Interlocked.Read(ref _receivedApplicationMessagesCount);
-        
-        public long SentPacketsCount => Interlocked.Read(ref _sentPacketsCount);
-        
+
         public long ReceivedPacketsCount => Interlocked.Read(ref _receivedPacketsCount);
-        
+
+        public long SentApplicationMessagesCount => Interlocked.Read(ref _sentApplicationMessagesCount);
+
+        public long SentPacketsCount => Interlocked.Read(ref _sentPacketsCount);
+
         public void HandleReceivedPacket(MqttPacket packet)
         {
             if (packet == null)
             {
                 throw new ArgumentNullException(nameof(packet));
             }
-            
+
             // This class is tracking all values from Clients perspective!
             LastPacketSentTimestamp = DateTime.UtcNow;
 
@@ -73,20 +73,14 @@ namespace MQTTnet.Server
                 LastNonKeepAlivePacketReceivedTimestamp = LastPacketReceivedTimestamp;
             }
         }
-        public void ResetStatistics(){
-            Interlocked.Exchange(ref _sentApplicationMessagesCount,0);
-            Interlocked.Exchange(ref _receivedApplicationMessagesCount,0);
-            Interlocked.Exchange(ref _sentPacketsCount,0);
-            Interlocked.Exchange(ref _receivedPacketsCount,0);
-        }
-        
+
         public void HandleSentPacket(MqttPacket packet)
         {
             if (packet == null)
             {
                 throw new ArgumentNullException(nameof(packet));
             }
-            
+
             // This class is tracking all values from Clients perspective!
             LastPacketReceivedTimestamp = DateTime.UtcNow;
 
@@ -96,6 +90,14 @@ namespace MQTTnet.Server
             {
                 Interlocked.Increment(ref _receivedApplicationMessagesCount);
             }
+        }
+
+        public void ResetStatistics()
+        {
+            Interlocked.Exchange(ref _sentApplicationMessagesCount, 0);
+            Interlocked.Exchange(ref _receivedApplicationMessagesCount, 0);
+            Interlocked.Exchange(ref _sentPacketsCount, 0);
+            Interlocked.Exchange(ref _receivedPacketsCount, 0);
         }
     }
 }

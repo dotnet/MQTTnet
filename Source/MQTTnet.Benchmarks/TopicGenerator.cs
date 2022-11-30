687dafbae5f75575bec2079053fab00eddcac9e9
Christian <6939810+chkr1011@users.noreply.github.com>
2022-11-30 18:00:35 +0100

    Refactor benchmarks

--- Source/MQTTnet.Benchmarks/TopicGenerator.cs
+++ Source/MQTTnet.Benchmarks/TopicGenerator.cs
@@ -1,31 +1,30 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace MQTTnet.Benchmarks
 {
     public class TopicGenerator
     {
         public static void Generate(
-           int numPublishers, int numTopicsPerPublisher,
-           out Dictionary<string, List<string>> topicsByPublisher,
-           out Dictionary<string, List<string>> singleWildcardTopicsByPublisher,
-           out Dictionary<string, List<string>> multiWildcardTopicsByPublisher
-           )
+            int numPublishers,
+            int numTopicsPerPublisher,
+            out Dictionary<string, List<string>> topicsByPublisher,
+            out Dictionary<string, List<string>> singleWildcardTopicsByPublisher,
+            out Dictionary<string, List<string>> multiWildcardTopicsByPublisher)
         {
             topicsByPublisher = new Dictionary<string, List<string>>();
             singleWildcardTopicsByPublisher = new Dictionary<string, List<string>>();
             multiWildcardTopicsByPublisher = new Dictionary<string, List<string>>();
 
             // Find some reasonable distribution across three topic levels
-            var topicsPerLevel = (int)Math.Pow(numTopicsPerPublisher, (1.0 / 3.0));
+            var topicsPerLevel = (int)Math.Pow(numTopicsPerPublisher, 1.0 / 3.0);
             if (topicsPerLevel <= 0)
             {
                 topicsPerLevel = 1;
             }
 
-            int numLevel1Topics = topicsPerLevel;
-            int numLevel2Topics = topicsPerLevel;
+            var numLevel1Topics = topicsPerLevel;
+            var numLevel2Topics = topicsPerLevel;
 
             var maxNumLevel3Topics = 1 + (int)((double)numTopicsPerPublisher / numLevel1Topics / numLevel2Topics);
             if (maxNumLevel3Topics <= 0)
@@ -35,7 +34,7 @@ namespace MQTTnet.Benchmarks
 
             for (var p = 0; p < numPublishers; ++p)
             {
-                int publisherTopicCount = 0;
+                var publisherTopicCount = 0;
                 var publisherName = "pub" + p;
                 for (var l1 = 0; l1 < numLevel1Topics; ++l1)
                 {
@@ -44,19 +43,22 @@ namespace MQTTnet.Benchmarks
                         for (var l3 = 0; l3 < maxNumLevel3Topics; ++l3)
                         {
                             if (publisherTopicCount >= numTopicsPerPublisher)
+                            {
                                 break;
+                            }
 
-                            var topic = string.Format("{0}/building{1}/level{2}/sensor{3}", publisherName, l1 + 1, l2 + 1, l3 + 1);
+                            var topic = $"{publisherName}/building{l1 + 1}/level{l2 + 1}/sensor{l3 + 1}";
                             AddPublisherTopic(publisherName, topic, topicsByPublisher);
 
                             if (l2 == 0)
                             {
-                                var singleWildcardTopic = string.Format("{0}/building{1}/+/sensor{2}", publisherName, l1 + 1, l3 + 1);
+                                var singleWildcardTopic = $"{publisherName}/building{l1 + 1}/+/sensor{l3 + 1}";
                                 AddPublisherTopic(publisherName, singleWildcardTopic, singleWildcardTopicsByPublisher);
                             }
-                            if ((l1 == 0) && (l3 == 0))
+
+                            if (l1 == 0 && l3 == 0)
                             {
-                                var multiWildcardTopic = string.Format("{0}/+/level{1}/+", publisherName, l2 + 1);
+                                var multiWildcardTopic = $"{publisherName}/+/level{l2 + 1}/+";
                                 AddPublisherTopic(publisherName, multiWildcardTopic, multiWildcardTopicsByPublisher);
                             }
 
@@ -75,6 +77,7 @@ namespace MQTTnet.Benchmarks
                 topicList = new List<string>();
                 topicsByPublisher.Add(publisherName, topicList);
             }
+
             topicList.Add(topic);
         }
     }

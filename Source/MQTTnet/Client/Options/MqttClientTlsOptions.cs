b7f99699b24e3f3acb9a798c75c8c4965204ca07
Jeff Scherrer <jeff@mushmo.com>
2022-12-24 20:45:39 -0800

    Added dotnet 7 support/builds.

--- Source/MQTTnet/Client/Options/MqttClientTlsOptions.cs
+++ Source/MQTTnet/Client/Options/MqttClientTlsOptions.cs
@@ -35,7 +35,7 @@ namespace MQTTnet.Client
         public System.Net.Security.CipherSuitesPolicy CipherSuitesPolicy { get; set; }
 #endif
 
-#if NET48 || NETCOREAPP3_1 || NET5 || NET6
+#if NET48 || NETCOREAPP3_1 || NET5 || NET6 || NET7_0
         public SslProtocols SslProtocol { get; set; } = SslProtocols.Tls12 | SslProtocols.Tls13;
 #else
         public SslProtocols SslProtocol { get; set; } = SslProtocols.Tls12 | (SslProtocols)0x00003000 /*Tls13*/;

e01707d25e784afd0c97960e5d1cd278b0d8435e
Christian <6939810+chkr1011@users.noreply.github.com>
2023-01-28 13:08:08 +0100

    Improve samples

--- Source/MQTTnet.Tests/Server/Tls_Tests.cs
+++ Source/MQTTnet.Tests/Server/Tls_Tests.cs
@@ -1,10 +1,4 @@
-
 #if !WINDOWS_UWP && (NET48_OR_GREATER || NET5_0_OR_GREATER)
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MQTTnet.Certificates;
-using MQTTnet.Client;
-using MQTTnet.Server;
-using MQTTnet.Tests.Mockups;
 using System;
 using System.Linq;
 using System.Net;
@@ -13,61 +7,52 @@ using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MQTTnet.Certificates;
+using MQTTnet.Client;
+using MQTTnet.Formatter;
+using MQTTnet.Server;
+using MQTTnet.Tests.Mockups;
 
 namespace MQTTnet.Tests.Server
 {
-
     [TestClass]
-    
-    public class Tls_Tests : BaseTestClass
+    public sealed class Tls_Tests : BaseTestClass
     {
-
-        public X509Certificate2 CreateCertificate(string oid)
+        static X509Certificate2 CreateCertificate(string oid)
         {
             var sanBuilder = new SubjectAlternativeNameBuilder();
             sanBuilder.AddIpAddress(IPAddress.Loopback);
             sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
             sanBuilder.AddDnsName("localhost");
-            var rsa = RSA.Create();
-
-            var certRequest = new CertificateRequest("CN=localhost", rsa, HashAlgorithmName.SHA512, RSASignaturePadding.Pkcs1);
-
-            certRequest.CertificateExtensions.Add(
-                new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));
-
-            certRequest.CertificateExtensions.Add(
-                new X509EnhancedKeyUsageExtension(
-                    new OidCollection { new Oid(oid) }, false));
-
-            certRequest.CertificateExtensions.Add(sanBuilder.Build());
-
-            var certificate = certRequest.CreateSelfSigned(DateTimeOffset.Now.AddMinutes(-10), DateTimeOffset.Now.AddMinutes(10));
-            var pfxCertificate = new X509Certificate2(certificate.Export(X509ContentType.Pfx), (string)null, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.Exportable);
             
-            return pfxCertificate;
-        }
+            using (var rsa = RSA.Create())
+            {
+                var certRequest = new CertificateRequest("CN=localhost", rsa, HashAlgorithmName.SHA512, RSASignaturePadding.Pkcs1);
 
-        private async Task<IMqttClient> ConnectClientAsync(TestEnvironment testEnvironment, Func<MqttClientCertificateValidationEventArgs, bool> certValidator)
-        {
-            var clientOptionsBuilder = testEnvironment.Factory.CreateClientOptionsBuilder();
-            clientOptionsBuilder
-                .WithClientId(Guid.NewGuid().ToString())
-                .WithTcpServer("localhost", 8883)
-                .WithTls(tls =>
+                certRequest.CertificateExtensions.Add(
+                    new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));
+
+                certRequest.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(new OidCollection { new Oid(oid) }, false));
+
+                certRequest.CertificateExtensions.Add(sanBuilder.Build());
+
+                using (var certificate = certRequest.CreateSelfSigned(DateTimeOffset.Now.AddMinutes(-10), DateTimeOffset.Now.AddMinutes(10)))
                 {
-                    tls.UseTls = true;
-                    tls.SslProtocol = SslProtocols.Tls12;
-                    tls.CertificateValidationHandler = certValidator;
-                });
+                    var pfxCertificate = new X509Certificate2(
+                        certificate.Export(X509ContentType.Pfx),
+                        (string)null,
+                        X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.Exportable);
 
-            var clientOptions = clientOptionsBuilder.Build();
-            return await testEnvironment.ConnectClient(clientOptions);
+                    return pfxCertificate;
+                }
+            }
         }
 
         [TestMethod]
         public async Task Tls_Swap_Test()
         {
-            var testEnvironment = CreateTestEnvironment(MQTTnet.Formatter.MqttProtocolVersion.V500);
+            var testEnvironment = CreateTestEnvironment(MqttProtocolVersion.V500);
             var serverOptionsBuilder = testEnvironment.Factory.CreateServerOptionsBuilder();
 
             var firstOid = "1.3.6.1.5.5.7.3.1";
@@ -78,11 +63,7 @@ namespace MQTTnet.Tests.Server
                 CurrentCertificate = CreateCertificate(firstOid)
             };
 
-            serverOptionsBuilder
-                .WithoutDefaultEndpoint()
-                .WithEncryptedEndpoint()
-                .WithEncryptionSslProtocol(SslProtocols.Tls12)
-                .WithEncryptionCertificate(certificateProvider);
+            serverOptionsBuilder.WithoutDefaultEndpoint().WithEncryptedEndpoint().WithEncryptionSslProtocol(SslProtocols.Tls12).WithEncryptionCertificate(certificateProvider);
 
             var serverOptions = serverOptionsBuilder.Build();
 
@@ -97,12 +78,14 @@ namespace MQTTnet.Tests.Server
             };
 
             await server.StartAsync();
-     
-            var firstClient = await ConnectClientAsync(testEnvironment, args =>
-            {
-                Assert.AreEqual(firstOid, ((X509Certificate2)args.Certificate).Extensions.OfType<X509EnhancedKeyUsageExtension>().First().EnhancedKeyUsages[0].Value);
-                return true;
-            });
+
+            var firstClient = await ConnectClientAsync(
+                testEnvironment,
+                args =>
+                {
+                    Assert.AreEqual(firstOid, ((X509Certificate2)args.Certificate).Extensions.OfType<X509EnhancedKeyUsageExtension>().First().EnhancedKeyUsages[0].Value);
+                    return true;
+                });
 
             var firstClientReceivedCount = 0;
             firstClient.ApplicationMessageReceivedAsync += args =>
@@ -114,27 +97,31 @@ namespace MQTTnet.Tests.Server
 
             await firstClient.SubscribeAsync("TestTopic1");
 
-            await firstClient.PublishAsync(new MqttApplicationMessage
-                {
-                    Topic = "TestTopic1",
-                    Payload = new byte[] { 1, 2, 3, 4}
-                });
-
-            await testEnvironment.Server.InjectApplicationMessage(new InjectedMqttApplicationMessage(
+            await firstClient.PublishAsync(
                 new MqttApplicationMessage
                 {
                     Topic = "TestTopic1",
                     Payload = new byte[] { 1, 2, 3, 4 }
-                }));
+                });
+
+            await testEnvironment.Server.InjectApplicationMessage(
+                new InjectedMqttApplicationMessage(
+                    new MqttApplicationMessage
+                    {
+                        Topic = "TestTopic1",
+                        Payload = new byte[] { 1, 2, 3, 4 }
+                    }));
 
             certificateProvider.CurrentCertificate = CreateCertificate(secondOid);
 
             // Validate that the certificate was switched
-            var secondClient = await ConnectClientAsync(testEnvironment, args =>
-            {
-                Assert.AreEqual(secondOid, ((X509Certificate2)args.Certificate).Extensions.OfType<X509EnhancedKeyUsageExtension>().First().EnhancedKeyUsages[0].Value);
-                return true;
-            });
+            var secondClient = await ConnectClientAsync(
+                testEnvironment,
+                args =>
+                {
+                    Assert.AreEqual(secondOid, ((X509Certificate2)args.Certificate).Extensions.OfType<X509EnhancedKeyUsageExtension>().First().EnhancedKeyUsages[0].Value);
+                    return true;
+                });
 
             var secondClientReceivedCount = 0;
             secondClient.ApplicationMessageReceivedAsync += args =>
@@ -146,32 +133,36 @@ namespace MQTTnet.Tests.Server
 
             await secondClient.SubscribeAsync("TestTopic2");
 
-            await firstClient.PublishAsync(new MqttApplicationMessage
+            await firstClient.PublishAsync(
+                new MqttApplicationMessage
                 {
                     Topic = "TestTopic2",
                     Payload = new byte[] { 1, 2, 3, 4 }
                 });
 
-            await testEnvironment.Server.InjectApplicationMessage(new InjectedMqttApplicationMessage(
-                new MqttApplicationMessage
-                {
-                    Topic = "TestTopic2",
-                    Payload = new byte[] { 1, 2, 3, 4 }
-                }));
+            await testEnvironment.Server.InjectApplicationMessage(
+                new InjectedMqttApplicationMessage(
+                    new MqttApplicationMessage
+                    {
+                        Topic = "TestTopic2",
+                        Payload = new byte[] { 1, 2, 3, 4 }
+                    }));
 
             // Ensure first client still works
-            await firstClient.PublishAsync(new MqttApplicationMessage
-            {
-                Topic = "TestTopic1",
-                Payload = new byte[] { 1, 2, 3, 4 }
-            });
-
-            await testEnvironment.Server.InjectApplicationMessage(new InjectedMqttApplicationMessage(
+            await firstClient.PublishAsync(
                 new MqttApplicationMessage
                 {
                     Topic = "TestTopic1",
                     Payload = new byte[] { 1, 2, 3, 4 }
-                }));
+                });
+
+            await testEnvironment.Server.InjectApplicationMessage(
+                new InjectedMqttApplicationMessage(
+                    new MqttApplicationMessage
+                    {
+                        Topic = "TestTopic1",
+                        Payload = new byte[] { 1, 2, 3, 4 }
+                    }));
 
             await Task.Delay(1000);
 
@@ -182,17 +173,32 @@ namespace MQTTnet.Tests.Server
             await server.StopAsync();
         }
 
-
-        private class CertificateProvider : ICertificateProvider
+        static async Task<IMqttClient> ConnectClientAsync(TestEnvironment testEnvironment, Func<MqttClientCertificateValidationEventArgs, bool> certValidator)
         {
+            var clientOptionsBuilder = testEnvironment.Factory.CreateClientOptionsBuilder();
+            clientOptionsBuilder.WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer("localhost", 8883)
+                .WithTls(
+                    tls =>
+                    {
+                        tls.UseTls = true;
+                        tls.SslProtocol = SslProtocols.Tls12;
+                        tls.CertificateValidationHandler = certValidator;
+                    });
 
+            var clientOptions = clientOptionsBuilder.Build();
+            return await testEnvironment.ConnectClient(clientOptions);
+        }
+        
+        sealed class CertificateProvider : ICertificateProvider
+        {
             public X509Certificate2 CurrentCertificate { get; set; }
 
             public X509Certificate2 GetCertificate()
-                => CurrentCertificate;
-
+            {
+                return CurrentCertificate;
+            }
         }
-
     }
 }
 #endif

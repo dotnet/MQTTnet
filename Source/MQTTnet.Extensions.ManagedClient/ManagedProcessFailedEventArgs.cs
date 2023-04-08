59d99509c896e27d0b417dc986ce8f4fa849fc06
scottbrogden-iheartmedia <74422469+scottbrogden-iheartmedia@users.noreply.github.com>
2023-04-08 06:03:58 -0600

    Notify what subscription topics failed as part of the SynchronizingSu… (#1678)
    * Notify what subscription topics failed as part of the SynchronizingSubscriptionsFailedAsync callback
    
    * Update ReleaseNotes.md
    
    ---------
    
    Co-authored-by: Christian <6939810+chkr1011@users.noreply.github.com>

--- Source/MQTTnet.Extensions.ManagedClient/ManagedProcessFailedEventArgs.cs
+++ Source/MQTTnet.Extensions.ManagedClient/ManagedProcessFailedEventArgs.cs
@@ -2,17 +2,33 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using MQTTnet.Packets;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MQTTnet.Extensions.ManagedClient
 {
     public class ManagedProcessFailedEventArgs : EventArgs
     {
-        public ManagedProcessFailedEventArgs(Exception exception)
+        public ManagedProcessFailedEventArgs(Exception exception, List<MqttTopicFilter> addedSubscriptions, List<string> removedSubscriptions)
         {
             Exception = exception ?? throw new ArgumentNullException(nameof(exception));
+
+            if (addedSubscriptions != null)
+            {
+                AddedSubscriptions = new List<string>(addedSubscriptions.Select(item => item.Topic));
+            }
+
+            if (removedSubscriptions != null)
+            {
+                RemovedSubscriptions = new List<string>(removedSubscriptions);
+            }
         }
 
         public Exception Exception { get; }
+
+        public List<string> AddedSubscriptions { get; }
+        public List<string> RemovedSubscriptions { get; }
     }
 }
